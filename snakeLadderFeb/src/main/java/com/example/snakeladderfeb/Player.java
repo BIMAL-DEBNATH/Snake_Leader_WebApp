1832b3767d6949228a03e10770474495d48d8c4a
BIMAL DEBNATH <131388946+BIMAL-DEBNATH@users.noreply.github.com>
2023-05-04 19:25:43 +0530

    Add files via upload

---
+++ snakeLadderFeb/src/main/java/com/example/snakeladderfeb/Player.java
@@ -0,0 +1,88 @@
+package com.example.snakeladderfeb;
+import javafx.animation.PauseTransition;
+import javafx.animation.SequentialTransition;
+import javafx.util.Duration;
+import javafx.animation.TranslateTransition;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+//import java.time.Duration;
+
+import static java.time.Duration.*;
+
+public class Player {
+   private Circle coin;
+    private int currentPosition;
+    private String name;
+
+    private static Board gameBoard=new Board();
+
+    public Player(int tileSize, Color coinColor, String playerName){
+        coin=new Circle(tileSize /2);
+        coin.setFill(coinColor);
+        currentPosition=0;
+        movePlayer(1);
+        name=playerName;
+    }
+
+    public void movePlayer(int diceValue) {
+        if (currentPosition + diceValue <= 100) {
+           currentPosition += diceValue;
+
+           TranslateTransition secondMove=null, firstMove=translateAnimation(diceValue);
+
+
+            int newPosition=gameBoard.getNewPosition(currentPosition);
+            if(newPosition !=currentPosition && newPosition!=-1){
+                 currentPosition=newPosition;
+               secondMove= translateAnimation(6);
+            }
+
+            if(secondMove==null){
+                firstMove.play();
+            }else{
+                SequentialTransition sequentialTransition=new SequentialTransition(firstMove,
+                new PauseTransition(Duration.millis(500)), secondMove);
+                sequentialTransition.play();
+            }
+
+        }
+    }
+
+    //for moving coin slow motion;
+    private  TranslateTransition translateAnimation(int diceValue){
+        //alternative
+       TranslateTransition animate = new TranslateTransition(Duration.millis(100*diceValue), coin);
+        animate.setToX(gameBoard.getXCoordinate(currentPosition));
+        animate.setToY(gameBoard.getYCoordinate(currentPosition));
+        animate.setAutoReverse(false);
+       return animate;
+    }
+
+    public  void startingPosition(){
+        currentPosition=0;
+        movePlayer(1);
+    }
+
+    boolean isWinner(){
+
+        if(currentPosition==100){
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+
+
+    public Circle getCoin() {
+        return coin;
+    }
+
+    public int getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
